d55a285889482167b8b696340f00d15689c2f63d
Satyaki Bose <satyakibose98@gmail.com>
2020-09-17 15:36:49 +0530

    Toolbar, Sidedrawer Responsive adjustment

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Toolbar.css';
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
-            <NavigationItems />
-        </nav>
-    </header>
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
 );
 
 export default toolbar;
